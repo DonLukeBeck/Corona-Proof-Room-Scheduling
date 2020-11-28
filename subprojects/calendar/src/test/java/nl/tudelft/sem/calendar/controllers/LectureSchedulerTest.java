0be37772c953092660595d73768ddaf3b51468e8
Matthijs de Goede <m.b.j.degoede@student.tudelft.nl>
2020-11-28 14:22:26 +0100

    Refactor test for the LectureScheduler

--- subprojects/calendar/src/test/java/nl/tudelft/sem/calendar/controllers/LectureSchedulerTest.java
+++ subprojects/calendar/src/test/java/nl/tudelft/sem/calendar/controllers/LectureSchedulerTest.java
@@ -19,25 +19,12 @@ class LectureSchedulerTest {
  private static LocalTime endTime;
  private static int timeGapLengthInMinutes;
 
- private static Room room1;
- private static Room room2;
- private static Room room3;
- private static Room room4;
- private static Room room5;
-
- private static RequestedLecture lecture1;
- private static RequestedLecture lecture2;
- private static RequestedLecture lecture3;
- private static RequestedLecture lecture4;
- private static RequestedLecture lecture5;
-
- private static Course course1;
- private static Course course2;
- private static Course course3;
-
- private static Date date1;
- private static Date date2;
- private static Date date3;
+ // Objects used in the test cases
+ private static Room[] testRooms;
+ private static RequestedLecture[] testReqLectures;
+ private static ScheduledLecture[] testSchedLectures;
+ private static Course[] testCourses;
+ private static Date[]  testDates;
 
     @BeforeEach
     void setup(){
@@ -48,44 +35,56 @@ class LectureSchedulerTest {
         timeGapLengthInMinutes = 45;
         startTime = LocalTime.of(8, 45);
         endTime = LocalTime.of(17, 45);
-        scheduler = new LectureScheduler(roomList, lecturesToSchedule, startTime, endTime, timeGapLengthInMinutes);
+        scheduler = new LectureScheduler(roomList, lecturesToSchedule,
+                startTime, endTime, timeGapLengthInMinutes);
     }
 
     private static void createRooms(){
-        room1 = new Room(1, 10, "Class");
-        room2 = new Room(2, 20, "IZ-2");
-        room3 = new Room(3, 30, "W-2");
-        room4 = new Room(4, 15, "IZ-4");
-        room5 = new Room(5, 25, "TS-3");
-        roomList = Arrays.asList(room1, room2, room3, room4, room5);
+        testRooms = new Room[5];
+        testRooms[0] = new Room(1, 10, "Class");
+        testRooms[1] = new Room(2, 20, "IZ-2");
+        testRooms[2] = new Room(3, 30, "W-2");
+        testRooms[3] = new Room(4, 15, "IZ-4");
+        testRooms[4] = new Room(5, 25, "TS-3");
+        roomList = Arrays.asList(testRooms[0], testRooms[1],
+                testRooms[2], testRooms[3], testRooms[4]);
     }
 
     private static void createCourses(){
-        course1 = new Course(Arrays.asList("abobe","mbjdegoede"));
-        course2 = new Course(Arrays.asList("someNetId","someNetId2","slokn"));
-        course3 = new Course(Arrays.asList("mdavid"));
+        testCourses = new Course[3];
+        testCourses[0] = new Course(Arrays.asList("abobe","mbjdegoede"));
+        testCourses[1] = new Course(Arrays.asList("someNetId","someNetId2","slokn"));
+        testCourses[2] = new Course(Arrays.asList("mdavid"));
     }
 
     private static void createDates(){
-        date1 = new GregorianCalendar(2020, Calendar.FEBRUARY, 1).getTime();
-        date2 = new GregorianCalendar(2020, Calendar.FEBRUARY, 13).getTime();
-        date3 = new GregorianCalendar(2020, Calendar.FEBRUARY, 15).getTime();
+        testDates = new Date[3];
+        testDates[0] = new GregorianCalendar(2020, Calendar.FEBRUARY, 1).getTime();
+        testDates[1] = new GregorianCalendar(2020, Calendar.FEBRUARY, 13).getTime();
+        testDates[2] = new GregorianCalendar(2020, Calendar.FEBRUARY, 15).getTime();
     }
 
     private static void createLectures(){
-        lecture1 = new RequestedLecture(course1, date1, 90);
-        lecture2 = new RequestedLecture(course2, date1, 200);
-        lecture3 = new RequestedLecture(course3, date1, 100);
-        lecture4 = new RequestedLecture(course1, date2, 90);
-        lecture5 = new RequestedLecture(course1, date3, 200);
-        lecturesToSchedule = Arrays.asList(lecture1, lecture5, lecture3, lecture4, lecture2);
+        testReqLectures = new RequestedLecture[5];
+        testReqLectures[0] = new RequestedLecture(testCourses[0], testDates[0], 90);
+        testReqLectures[1] = new RequestedLecture(testCourses[1], testDates[0], 200);
+        testReqLectures[2] = new RequestedLecture(testCourses[2], testDates[0], 100);
+        testReqLectures[3] = new RequestedLecture(testCourses[0], testDates[1], 90);
+        testReqLectures[4] = new RequestedLecture(testCourses[0], testDates[2], 200);
+        lecturesToSchedule = Arrays.asList(testReqLectures[0], testReqLectures[1],
+                testReqLectures[2], testReqLectures[3], testReqLectures[4]);
+
+        testSchedLectures = new ScheduledLecture[2];
+        testSchedLectures[0] = new ScheduledLecture(testCourses[0],testDates[0]);
+        testSchedLectures[1] = new ScheduledLecture(testCourses[1], testDates[0]);
     }
 
     private static Map<Date, List<RequestedLecture>> createMap(){
         return new HashMap<>() {{
-            put(date1, Arrays.asList(lecture1, lecture3, lecture2));
-            put(date2, Arrays.asList(lecture4));
-            put(date3, Arrays.asList(lecture5));
+            put(testDates[0], Arrays.asList(testReqLectures[0],
+                    testReqLectures[1], testReqLectures[2]));
+            put(testDates[1], Arrays.asList(testReqLectures[3]));
+            put(testDates[2], Arrays.asList(testReqLectures[4]));
         }};
     }
 
@@ -101,7 +100,8 @@ class LectureSchedulerTest {
     @Test
     void testSortRoomsByCapacity() {
         scheduler.sortRoomsByCapacity();
-        assertThat(scheduler.getRoomList()).containsExactly(room3,room5,room2,room4,room1);
+        assertThat(scheduler.getRoomList()).containsExactly(testRooms[2],testRooms[4],testRooms[1]
+                ,testRooms[3],testRooms[0]);
     }
 
     @Test
@@ -113,8 +113,10 @@ class LectureSchedulerTest {
 
     @Test
     void testGetSortedLecturesForDay() {
-        assertThat(scheduler.getSortedLecturesForDay(date1, createMap()))
-                .containsExactly(lecture2,lecture1,lecture3);
+        assertThat(scheduler.getSortedLecturesForDay(testDates[0], createMap()))
+                .containsExactly(testReqLectures[1],testReqLectures[0],testReqLectures[2]);
+        assertThat(scheduler.getSortedLecturesForDay(testDates[1], createMap()))
+                .containsExactly(testReqLectures[3]);
     }
 
     @Test
@@ -122,55 +124,56 @@ class LectureSchedulerTest {
     }
 
     @Test
-    void testAssignFirstRoom() {
-        ScheduledLecture scheduledLecture1 = new ScheduledLecture(course1,date1);
+    void testAssignFirstLectureRoom() {
         scheduler.sortRoomsByCapacity();
-        scheduler.assignRoom(0,scheduledLecture1,lecture1.getDurationInMinutes());
-        assertThat(scheduledLecture1.getRoom()).isEqualTo(room3);
+        scheduler.assignRoom(0,testSchedLectures[0],
+                testReqLectures[0].getDurationInMinutes());
+        assertThat(testSchedLectures[0].getRoom()).isEqualTo(testRooms[2]);
+    }
+
+    @Test
+    void testAssignFirstLectureNoRoomLeft() {
+        scheduler.sortRoomsByCapacity();
+        scheduler.assignRoom(roomList.size(),testSchedLectures[0],
+                testReqLectures[0].getDurationInMinutes());
+        assertThat(testSchedLectures[0].getRoom()).isNull();
     }
 
     @Test
     void testAssignSecondLectureSameRoomWithinTime() {
-        ScheduledLecture scheduledLecture1 = new ScheduledLecture(course1,date1);
         scheduler.sortRoomsByCapacity();
-        scheduler.assignRoom(0,scheduledLecture1,lecture1.getDurationInMinutes());
-        scheduler.assignRoom(0,scheduledLecture1,lecture2.getDurationInMinutes());
-        assertThat(scheduledLecture1.getRoom()).isEqualTo(room3);
+        scheduler.assignRoom(0,testSchedLectures[0],
+                testReqLectures[0].getDurationInMinutes());
+        scheduler.assignRoom(0,testSchedLectures[1],
+                testReqLectures[1].getDurationInMinutes());
+        assertThat(testSchedLectures[0].getRoom()).isEqualTo(testRooms[2]);
     }
 
     @Test
     void testAssignSecondLectureSameRoomNotWithinTime() {
-        ScheduledLecture scheduledLecture1 = new ScheduledLecture(course1,date1);
         scheduler.sortRoomsByCapacity();
-        scheduler.assignRoom(0,scheduledLecture1,540);
-        scheduler.assignRoom(0,scheduledLecture1,lecture1.getDurationInMinutes());
-        assertThat(scheduledLecture1.getRoom()).isEqualTo(room5);
+        scheduler.assignRoom(0,testSchedLectures[0],540);
+        scheduler.assignRoom(0,testSchedLectures[1],
+                testReqLectures[1].getDurationInMinutes());
+        assertThat(testSchedLectures[1].getRoom()).isEqualTo(testRooms[4]);
     }
 
     @Test
     void testAssignSecondLectureSameRoomJustWithinTime() {
-        ScheduledLecture scheduledLecture1 = new ScheduledLecture(course1,date1);
         scheduler.sortRoomsByCapacity();
-        scheduler.assignRoom(0,scheduledLecture1,295);
-        scheduler.assignRoom(0,scheduledLecture1,lecture1.getDurationInMinutes());
-        assertThat(scheduledLecture1.getRoom()).isEqualTo(room3);
+        scheduler.assignRoom(0,testSchedLectures[0],185);
+        scheduler.assignRoom(0,testSchedLectures[1],
+                testReqLectures[1].getDurationInMinutes());
+        assertThat(testSchedLectures[1].getRoom()).isEqualTo(testRooms[2]);
     }
 
     @Test
-    void testNoRoomLeft() {
-        ScheduledLecture scheduledLecture1 = new ScheduledLecture(course1,date1);
+    void testAssignSecondLectureNoRoomLeft() {
         scheduler.sortRoomsByCapacity();
-        scheduler.assignRoom(roomList.size(),scheduledLecture1,lecture1.getDurationInMinutes());
-        assertThat(scheduledLecture1.getRoom()).isNull();
-    }
-
-    @Test
-    void testSecondLectureNoRoomLeft() {
-        ScheduledLecture scheduledLecture1 = new ScheduledLecture(course1,date1);
-        ScheduledLecture scheduledLecture2 = new ScheduledLecture(course1,date1);
-        scheduler.sortRoomsByCapacity();
-        scheduler.assignRoom(roomList.size()-1,scheduledLecture1,540);
-        scheduler.assignRoom(roomList.size()-1,scheduledLecture2,lecture1.getDurationInMinutes());
-        assertThat(scheduledLecture2.getRoom()).isNull();
+        scheduler.assignRoom(roomList.size()-1,
+                testSchedLectures[0],540);
+        scheduler.assignRoom(roomList.size()-1,
+                testSchedLectures[1],testReqLectures[1].getDurationInMinutes());
+        assertThat(testSchedLectures[1].getRoom()).isNull();
     }
 }
