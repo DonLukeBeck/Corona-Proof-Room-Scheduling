6b14b9d9fad231443bf1dea0093f3c0b60dc816b
Matthijs de Goede <m.b.j.degoede@student.tudelft.nl>
2020-11-29 18:39:30 +0100

    Fix typo

--- subprojects/calendar/src/test/java/nl/tudelft/sem/calendar/controllers/LectureSchedulerTest.java
+++ subprojects/calendar/src/test/java/nl/tudelft/sem/calendar/controllers/LectureSchedulerTest.java
@@ -7,7 +7,7 @@ import nl.tudelft.sem.calendar.entities.Room;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;W
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
