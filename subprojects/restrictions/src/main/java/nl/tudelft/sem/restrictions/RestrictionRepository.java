e97db061f1ee7e9c999ec014a085bc2bae9c4275
Matthijs de Goede <m.b.j.degoede@student.tudelft.nl>
2020-11-30 13:47:02 +0100

    Remove merged files from the Restriction and Room management. They are to be added in Cans merge request later on.

--- subprojects/restrictions/src/main/java/nl/tudelft/sem/restrictions/RestrictionRepository.java
+++
@@ -1,8 +0,0 @@
-package nl.tudelft.sem.restrictions;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestrictionRepository extends JpaRepository<Restriction, String> {
-}
