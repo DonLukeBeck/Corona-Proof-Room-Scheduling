02f3111d4cce6dec2ffa618853d10fdd4d09b8ab
canparlar <c.parlar@student.tudelft.nl>
2020-12-03 00:07:32 +0100

    add repository functions and made the tests that they do not need to connect to the database

--- subprojects/restrictions/src/main/java/nl/tudelft/sem/restrictions/RestrictionRepository.java
+++ subprojects/restrictions/src/main/java/nl/tudelft/sem/restrictions/RestrictionRepository.java
@@ -1,8 +1,12 @@
 package nl.tudelft.sem.restrictions;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface RestrictionRepository extends JpaRepository<Restriction, String> {
+public interface RestrictionRepository extends JpaRepository<Restriction, String>, JpaSpecificationExecutor<Restriction> {
+    Optional<Restriction> getRestrictionByName(String name);
 }
--- subprojects/restrictions/src/test/java/nl/tudelft/sem/restrictions/RestrictionControllerTest.java
+++ subprojects/restrictions/src/test/java/nl/tudelft/sem/restrictions/RestrictionControllerTest.java
@@ -1,41 +1,62 @@
 package nl.tudelft.sem.restrictions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
+
+@ContextConfiguration(classes = Restriction.class)
+@AutoConfigureMockMvc
+@WebMvcTest
+// This class doesn't ever need to be serialized, so neither do it's members
+@SuppressWarnings("PMD.BeanMembersShouldSerialize")
 class RestrictionControllerTest {
 
-    private transient Restriction rest1;
-    private transient Restriction rest2;
-    private transient RestrictionController restrictionController;
+    private Restriction rest1;
+    private Restriction rest2;
+    private String ae;
 
-    @Autowired
-    private transient RestrictionRepository restrictionRepository;
+    private RestrictionController restrictionController;
+
+    @MockBean
+    RestrictionRepository restrictionRepository;
 
     /**
      * The initial setup before each test.
      */
     @BeforeEach
     void setUp() {
-        rest1 = new Restriction();
-        rest1.setValue(1.0f);
-        rest1.setName("test");
-        restrictionRepository.save(rest1);
+        ae = "Already Exists";
+        this.rest1 = new Restriction();
+        this.rest1.setValue(1.0f);
+        this.rest1.setName("test");
 
-        rest2 = new Restriction();
-        rest2.setValue(2.0f);
-        rest2.setName("can");
-        restrictionRepository.save(rest2);
+        this.rest2 = new Restriction("test2", 2.0f);
 
         restrictionController = new RestrictionController(restrictionRepository);
+        when(restrictionRepository.getRestrictionByName(rest1.getName()))
+                .thenReturn(java.util.Optional.ofNullable(rest1));
+        when(restrictionRepository.getRestrictionByName(rest2.getName()))
+                .thenReturn(java.util.Optional.ofNullable(rest2));
+        when(restrictionRepository.getRestrictionByName("bigRoomMaxPercentage"))
+                .thenReturn(java.util.Optional.ofNullable(rest1));
+        when(restrictionRepository.getRestrictionByName("smallRoomMaxPercentage"))
+                .thenReturn(java.util.Optional.ofNullable(rest2));
+        when(restrictionRepository.getRestrictionByName("minSeatsBig"))
+                .thenReturn(java.util.Optional.ofNullable(rest2));
+        when(restrictionRepository.getRestrictionByName("gapTimeInMinutes"))
+                .thenReturn(java.util.Optional.ofNullable(rest1));
     }
 
     @Test
@@ -55,7 +76,94 @@ class RestrictionControllerTest {
 
     @Test
     public void addNewRestrictionSuccess2() {
-        assertEquals("Already Exists", restrictionController.addNewRestriction("test2", 2.0f));
+        assertEquals(ae, restrictionController.addNewRestriction("test2", 2.0f));
     }
 
+    @Test
+    public void equalsTest() {
+        assertEquals(rest2, rest2);
+    }
+
+    @Test
+    public void notEqualsTest() {
+        assertNotEquals(restrictionController, rest1);
+    }
+
+    @Test
+    public void notEquals2() {
+        assertNotEquals(rest2, rest1);
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals(rest1.toString(), "Restriction(name=test, value=1.0)");
+    }
+
+    @Test
+    void getRestrictionVal1() {
+        assertEquals(restrictionController.getRestrictionVal("test"), 1.0f);
+    }
+
+    @Test
+    void getRestrictionVal2() {
+        assertThrows(NoSuchElementException.class, () -> {
+            restrictionController.getRestrictionVal("aaa");
+        });
+    }
+
+    @Test
+    void setCapacityRestriction() {
+        assertEquals(ae, restrictionController.setCapacityRestriction(true, 1.0f));
+    }
+
+    @Test
+    void setCapacityRestriction2() {
+        assertEquals(ae, restrictionController.setCapacityRestriction(false, 2.0f));
+    }
+
+    @Test
+    void setMinSeatsBig() {
+        assertEquals(ae, restrictionController.setMinSeatsBig(2.0f));
+    }
+
+    @Test
+    void setTimeGapLength() {
+        assertEquals(ae, restrictionController.setTimeGapLength(1.0f));
+    }
+
+    @Test
+    void getCapacityRestriction() {
+        assertEquals(1.0f, restrictionController.getCapacityRestriction(true));
+    }
+
+    @Test
+    void getCapacityRestriction2() {
+        assertEquals(2.0f, restrictionController.getCapacityRestriction(false));
+    }
+
+    @Test
+    void getMinSeatsBig() {
+        assertEquals(2.0f, restrictionController.getMinSeatsBig());
+    }
+
+    @Test
+    void getTimeGapLength() {
+        assertEquals(1.0f, restrictionController.getTimeGapLength());
+    }
+
+    @Test
+    void setStartTime() {
+    }
+
+    @Test
+    void setEndTime() {
+    }
+
+    @Test
+    void getStartTime() {
+    }
+
+    @Test
+    void getEndTime() {
+    }
 }
