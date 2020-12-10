28645429113bbef9595bb835198f616c017d6b12
m.durmus-2@student.tudelft.nl <m.durmus-2@student.tudelft.nl>
2020-12-10 16:38:52 +0100

    Implement findCourseID in Course Management Service: - Implement getCourseId(String teacherId) to find the courseID given the teacher. - Implement the other methods in this class to use the findBy<Specific value> to retrieve information out of the database more efficiently

--- subprojects/courses/src/main/java/nl/tudelft/sem/courses/CourseRepository.java
+++ subprojects/courses/src/main/java/nl/tudelft/sem/courses/CourseRepository.java
@@ -2,9 +2,17 @@ package nl.tudelft.sem.courses;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface CourseRepository extends JpaRepository<Course, String> {
+public interface CourseRepository extends JpaRepository<Course, String>,
+        JpaSpecificationExecutor<Course> {
+
+    Course findByCourseId(String courseId);
+
+    Course findByCourseName(String courseName);
+
+    Course findByCourseIdForTeacher(String teacherId);
+
 }
