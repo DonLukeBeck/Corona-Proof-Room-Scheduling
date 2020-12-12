47e764b961677d5fbdd7b91933495d8cb916a535
Luca Becheanu <l.becheanu@student.tudelft.nl>
2020-12-13 00:34:42 +0100

    Fix 3 errors creating bean with name 'xRepository'

---
+++ subprojects/courses/src/main/java/nl/tudelft/sem/courses/repository/LectureRepository.java
@@ -0,0 +1,20 @@
+package nl.tudelft.sem.courses.repository;
+
+import java.sql.Date;
+import java.util.List;
+import nl.tudelft.sem.courses.entity.Lecture;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LectureRepository extends JpaRepository<Lecture, Integer>,
+        JpaSpecificationExecutor<Lecture> {
+
+    List<Lecture> findByCourseId(String courseId);
+
+    @Query(value = "SELECT * FROM Lecture WHERE course_id = ?1 AND scheduled_date = ?2 LIMIT 1", nativeQuery = true)
+    Lecture findByCourseIdAndDate(String courseId, Date date);
+
+}
