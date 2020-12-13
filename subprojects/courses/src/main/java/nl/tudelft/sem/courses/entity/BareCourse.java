6f94adc8fb78f6141336c4dff46d157d90b38b7f
Matthijs de Goede <m.b.j.deGoede@student.tudelft.nl>
2020-12-13 23:04:07 +0100

    Adapt course service entities

--- subprojects/courses/src/main/java/nl/tudelft/sem/courses/entity/BareCourse.java
+++ subprojects/courses/src/main/java/nl/tudelft/sem/courses/entity/BareCourse.java
@@ -1,11 +1,14 @@
 package nl.tudelft.sem.courses.entity;
 
-import java.util.Date;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BareCourse {
     private static final long serialVersionUID = 1233464312341123464L;
     private String courseId;
