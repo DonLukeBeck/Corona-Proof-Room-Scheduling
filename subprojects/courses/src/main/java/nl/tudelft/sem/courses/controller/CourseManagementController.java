8112efe8d2f9f8a3df61a32ffbc1d3f0dd0540bd
Merdan Durmus <m.durmus-2@student.tudelft.nl>
2020-12-16 13:32:29 +0100

    merge courseController into CourseManagementController

--- subprojects/courses/src/main/java/nl/tudelft/sem/courses/controller/CourseManagementController.java
+++ subprojects/courses/src/main/java/nl/tudelft/sem/courses/controller/CourseManagementController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import nl.tudelft.sem.courses.entity.AddCourse;
 import nl.tudelft.sem.courses.entity.AddLecture;
+import nl.tudelft.sem.courses.entity.BareCourse;
 import nl.tudelft.sem.courses.entity.Course;
 import nl.tudelft.sem.courses.entity.Enrollment;
 import nl.tudelft.sem.courses.entity.Lecture;
@@ -20,12 +21,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -60,6 +64,50 @@ public class CourseManagementController {
             throws IOException, InterruptedException {
         JSONObject jwtInfo = jwtValidate.jwtValidate(request);
         return jwtInfo;
+    }
+
+    /**
+     * Get endpoint to retrieve all courses.
+     *
+     * @return A list of {@link BareCourse}s
+     */
+    @GetMapping("/courses")
+    @ResponseBody
+    public ResponseEntity<?> listCourses() {
+        return ResponseEntity.ok(courseRepository.findAll());
+    }
+
+    /**
+     * Get endpoint to a course using an id.
+     *
+     * @return the {@link BareCourse} with courseId as id
+     */
+    @GetMapping("/id/{id}")
+    @ResponseBody
+    public ResponseEntity<?> listCourses(@PathVariable("id") String id) {
+        var res = courseRepository.findById(id);
+        if (res.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(res.get());
+    }
+
+    /**
+     * Retrieves a course list based on teacher id.
+     *
+     * @param request request information
+     * @param id id of the teacher
+     * @return a list of courses based on teacher id
+     */
+    @GetMapping("/teacher/{id}")
+    @ResponseBody
+    public ResponseEntity<?> getCoursesTeacher(HttpServletRequest request,
+                                               @PathVariable("id") String id) {
+        List<Course> courseList = courseRepository.findAllByTeacherId(id);
+        if (courseList == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(courseList);
     }
 
     /**
