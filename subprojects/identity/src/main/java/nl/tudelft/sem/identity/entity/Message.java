a20eade4d7ec6f20fa5aa780fb67a24e28340cdb
Luca Becheanu <l.becheanu@student.tudelft.nl>
2020-12-17 18:10:58 +0100

    Add temporary solution for sending JSON responses

---
+++ subprojects/identity/src/main/java/nl/tudelft/sem/identity/entity/Message.java
@@ -0,0 +1,11 @@
+package nl.tudelft.sem.identity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Message {
+    private String message;
+}
+
