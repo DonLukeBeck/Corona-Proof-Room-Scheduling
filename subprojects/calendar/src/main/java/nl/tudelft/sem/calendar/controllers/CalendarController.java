287091552e345d331b6398d1fd821a737ca8ae44
Timen Zandbergen <t.zandbergen-2@student.tudelft.nl>
2020-12-18 06:51:51 +0100

    Fix validateRole function

--- subprojects/calendar/src/main/java/nl/tudelft/sem/calendar/controllers/CalendarController.java
+++ subprojects/calendar/src/main/java/nl/tudelft/sem/calendar/controllers/CalendarController.java
@@ -421,17 +421,15 @@ public class CalendarController {
      *
      * @return an error message if the user hasn't got the desired role, else its netId.
      */
-    public String validateRole(HttpServletRequest request, String role)
-            throws JSONException, IOException, InterruptedException {
-
-        JSONObject jwtInfo = JwtValidate.jwtValidate(request);
+    public String validateRole(HttpServletRequest request, String role) {
         try {
+            JSONObject jwtInfo = JwtValidate.jwtValidate(request);
             if (!jwtInfo.getString("role").equals(role)) {
                 return noAccessMessage;
             }
+            return jwtInfo.getString("netid");
         } catch (Exception e) {
             return noAccessMessage;
         }
-        return jwtInfo.getString("netid");
     }
 }
