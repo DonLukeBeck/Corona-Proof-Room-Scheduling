8b3793bd237c6f149d20fc7e38140900617490d1
Luca Becheanu <l.becheanu@student.tudelft.nl>
2020-12-18 17:24:11 +0100

    Fix test failure

--- subprojects/calendar/src/main/java/nl/tudelft/sem/calendar/scheduling/LectureScheduler.java
+++ subprojects/calendar/src/main/java/nl/tudelft/sem/calendar/scheduling/LectureScheduler.java
@@ -93,14 +93,16 @@ public class LectureScheduler {
             List<Lecture> toScheduleThisDay = getSortedLecturesForDay(date, lecturesByDay);
             for (Lecture toBeScheduled : toScheduleThisDay) {
 
+                //checkstyle complains about the distance between the variable and its first usage
+                //however, we cannot use it after the calls below because the method will fail
+                int capacity = assignRoom(toBeScheduled, toBeScheduled.getDurationInMinutes());
+
                 // solve a time conversion issue by adding an hour to the time before export
                 toBeScheduled.setStartTime(toBeScheduled.getStartTime().plusHours(1));
                 toBeScheduled.setEndTime(toBeScheduled.getEndTime().plusHours(1));
 
                 // save lecture in database and update it with a version including an id
                 toBeScheduled = lectureRepository.saveAndFlush(toBeScheduled);
-
-                int capacity = assignRoom(toBeScheduled, toBeScheduled.getDurationInMinutes());
 
                 // then assign students to this lecture with id
                 assignStudents(capacity, toBeScheduled, allParticipants);
